264c46664cd8510f1e8403d912a0910e3ece6e5d
wolf5x <wolf5x@wolf5x-X41.(none)>
2013-08-18 13:51:24 +0800

    Fixed: stockout/index didnot remember filter condition.

--- smpss/app/v_t/%%91^91D^91DC34C5%%footer.html.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2013-08-17 00:53:51
-         compiled from simpla/common/footer.html */ ?>
-</div>
-<script>$(document).ready(function(){if($("#goods_sn").length)$("#goods_sn").focus();})</script>
-</body>
-</html>
