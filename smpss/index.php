b80e32ae8ebf54d13efa3121c3ff94fa195adbdb
wolf5x <wolf5x@wolf5x-1215b>
2014-05-27 00:14:43 +0800

    Format code.

--- smpss/index.php
+++ smpss/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * 入口文件
  * SmPSS(Supermarket Purchase Sale Stock)
@@ -7,11 +8,12 @@ require_once("global.php");
 SlightPHP::setDebug(true);
 SlightPHP::setAppDir(ROOT_APP);
 SlightPHP::setSplitFlag("/");
-SDb::setConfigFile(ROOT_CONFIG. "/db.ini.php");
-if(($r=SlightPHP::run())===false){
-	header('HTTP/1.1 404 Not Found');
-	header('Status: 404 Not Found');
-	include './app/v/'.base_Constant::TEMP_DIR.'/common/404.html';
-}else{
-	echo $r;exit;
+SDb::setConfigFile(ROOT_CONFIG . "/db.ini.php");
+if (($r = SlightPHP::run()) === false) {
+    header('HTTP/1.1 404 Not Found');
+    header('Status: 404 Not Found');
+    include './app/v/' . base_Constant::TEMP_DIR . '/common/404.html';
+} else {
+    echo $r;
+    exit;
 }
